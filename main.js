61549c4553e49b1ac7ced51738b81af831fe9ed8
Imi Nkateko <shikuhelenkateko@gmail.com>
2024-10-25 01:00:39 +0200

    error handling done

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+const formEl = document.querySelector(".form");
+const inputEl = document.querySelector(".inputEl");
+const displayEl = document.querySelector(".weather-data");
 
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const  city = inputEl.value;
+
+    if (city === "") {   
+        displayEl.style.display = "block";
+        handleError("Please Enter City");     
+    }
+})
+
+
+
+
+function handleError(message) {
+   
+    const errorDisplay = document.createElement("p");
+    errorDisplay.classList.add("error");
+    errorDisplay.textContent = message
+     
+    displayEl.appendChild(errorDisplay);
+}
