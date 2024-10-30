a6e61345bb36b4619384f7c3a6a43e3f9e10f370
Imi Nkateko <shikuhelenkateko@gmail.com>
2024-10-31 00:19:26 +0200

    getting weather data from api

--- main.js
+++ main.js
@@ -26,18 +26,48 @@ formEl.addEventListener("submit", async event => {
 })
 
 async function getWeatherData(city) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`  
+  const apiUrl = 
+  `https://api.openweathermap.org/data/2.5/weather?q=
+  ${city}&appid=${apiKey}`;
+
+  const response = await fetch(apiUrl);
+
+  console.log(response)
+
+  if (!response.ok) {
+    throw new Error("Could not fetch weather data")
+  } 
+
+  return await response.json();
 }
 
 function displayWeatherInfo(data) {
 
+   const { name: city,main: {temp, humidity}, weather: [{description, id}]} = data;
+    
+    displayEl.textContent = "";
+    displayEl.style.display = "block";
+
+    const humidityDiplay = document.createElement("p");
+    const cityDisplay = document.createElement("h1");
+    
+    cityDisplay.classList.add("city");
+    humidityDiplay.classList.add("humidity");
+
+    cityDisplay.textContent = city;
+    humidityDiplay.textContent = humidity;
+
+    
+    displayEl.appendChild(cityDisplay);
+    displayEl.appendChild(humidityDiplay)
+
 }
 
-
-function handleError(message) {   
+function handleError(message) {
+   
     const errorDisplay = document.createElement("p");
     errorDisplay.classList.add("error");
     errorDisplay.textContent = message
-    displayEl.style.display = "block";
+     displayEl.style.display ="block"
     displayEl.appendChild(errorDisplay);
 }
